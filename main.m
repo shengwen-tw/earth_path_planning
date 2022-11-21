f1631c7b5b84edf4319dab6903c0d5a59f3d4543
Shengwen Cheng <c.shengwen.tw@gmail.com>
2022-11-22 00:54:25 +0800

    implement scanning plot of waypoints

--- main.m
+++ main.m
@@ -4,5 +4,21 @@ geo_planner.draw_earth();
 geo_planner.draw_waypoint(23.9037, 121.0794, 0)
 geo_planner.draw_waypoint(-25.2744, 133.7751, 0)
 
+if 0
+lat_start = -90;
+lat_end = 90;
+long_start = -180;
+long_end = 180;
+delta = 10;
+
+    for i = 1:(lat_end - lat_start) / delta
+        for j = 1:(long_end - long_start) / delta
+            lat = i * delta + lat_start;
+            long = j * delta + long_start;
+            geo_planner.draw_waypoint(lat, long, 0);
+        end
+    end
+end
+
 pause;
 close all;
